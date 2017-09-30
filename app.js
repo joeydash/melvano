3faf5a7ddf057a978ec29fe6db5ce4dc71130f93
joeydash <joydassudipta@gmail.com>
2017-09-30 11:16:43 +0530

    feat:error handling page

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-/*var managedb = require('./routes/managedb');*/
+var managedb = require('./routes/managedb');
 
 var app = express();
 
@@ -25,11 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('^\/users/', users);
-/*app.use('/managedb', managedb);*/
+app.use('/managedb', managedb);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Page Not Found');
   err.status = 404;
   next(err);
 });
--- routes/managedb.js
+++ routes/managedb.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+
+var url = "mongodb://localhost:27017/test";
 
 /* GET home page. */
 router.post('/check', function(req, res, next) {
@@ -8,6 +13,20 @@ router.post('/check', function(req, res, next) {
 });
 router.post('/update', function(req, res, next) {
     var context = { title: 'Melvano',tagline:"Come learn with us!" }
+    res.render('index',context );
+});
+router.post('/insert', function(req, res, next) {
+    var item = {
+        name: "joeydash"
+    }
+    mongo.connect(url,function (err,db) {
+        assert.equal(null,err);
+        db.collection('user_data').insertOne(item,function (error,result) {
+            assert.equal(null,error);
+            console.log("item inserted!");
+        });
+    });
+
     res.render('index',context );
 });
 

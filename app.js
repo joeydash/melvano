01093f0dc36c9ccd64776fea3dbda560e440203a
2ra3 <rahilshajahanabduljaffar@gmail.com>
2017-11-07 01:21:53 +0530

    added signup page,made changes in app.js,in routers created signup.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var test = require('./routes/test');
 var login = require('./routes/login');
+var signup = require('./routes/signup')
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', index);
 app.use('^\/users/', users);
 app.use('/test', test);
 app.use('/login', login);
+app.use('/signup',signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
